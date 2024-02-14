89464205ff350b71407abaf99d9b6efc98d248ba
ArCrz <arcecrz.06@gmail.com>
2024-02-14 11:01:46 -0600

    Se corrigieron las librerias, y se agregaron las varuables y se aplanaron

--- TAREA 2 Y 3/keras.py
+++ TAREA 2 Y 3/keras.py
@@ -1,11 +1,22 @@
 #Primero vamos a importar las librerías.
 import tensorflow as tf
-from tensorflow import keras 
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequentials
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras. optimizers import Adam, RMSprop, SGD
+from tensorflow import keras as keras 
+from keras.datasets import mnist
+from keras.models import Sequentials
+from keras.layers import Dense, Dropout
+from keras. optimizers import Adam, RMSprop, SGD
+from keras.regularizers import L1, L2, L1L2
+import numpy as np
 
+dataset= mnist.load_data() 
+(x_train, y_train), (x_test,y_test) = dataset #Son las variables separadas de los datos de entrenamiento y pruebas
 
+#Ahora aplanamos las imágenes y convertimos en punto flotante
+x_train = x_train.reshape(60000,784)
+x_testv = x_test.reshape(6000,784)
+x_train = x_train.astype('float32')
+x_testv = x_test.astype('float32')
 
-
+num_clases = 10
+y_trainc = keras.utils.tocategorical(y_train, num_clases)
+y_testc = keras.utils.tocategorical(y_test, num_clases)
