e35074bc1dd2486033f8317709364dab60a8077e
ArCrz <arcecrz.06@gmail.com>
2024-02-14 10:21:02 -0600

    se empezó el código

---
+++ TAREA 2 Y 3/keras.py
@@ -0,0 +1,11 @@
+#Primero vamos a importar las librerías.
+import tensorflow as tf
+from tensorflow import keras 
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequentials
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras. optimizers import Adam, RMSprop, SGD
+
+
+
+
