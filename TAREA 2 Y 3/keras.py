ea05bcb30f3b7c5b41f8381dd99fa1855ca37f88
ArCrz <arcecrz.06@gmail.com>
2024-02-14 11:36:35 -0600

    se corrigieron algunos errores ortográficos

--- TAREA 2 Y 3/keras.py
+++ TAREA 2 Y 3/keras.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 from tensorflow import keras as keras 
 from keras.datasets import mnist
-from keras.models import Sequentials
+from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras. optimizers import Adam, RMSprop, SGD
 from keras.regularizers import L1, L2, L1L2
@@ -13,21 +13,21 @@ dataset= mnist.load_data()
 
 #Ahora aplanamos las imágenes y convertimos en punto flotante
 x_train = x_train.reshape(60000,784)
-x_test = x_test.reshape(6000,784)
+x_test = x_test.reshape(10000,784)
 x_trainv = x_train.astype('float32')
 x_testv = x_test.astype('float32')
 
 lr = 0.001  #learning rate
 num_clases = 10
-y_trainc = keras.utils.tocategorical(y_train, num_clases)
-y_testc = keras.utils.tocategorical(y_test, num_clases)
+y_trainc = keras.utils.to_categorical(y_train, num_clases)
+y_testc = keras.utils.to_categorical(y_test, num_clases)
 
 #Ahora vayamos con la parte a) en ta tarea pasada traté de usar Adam y categorical cross entropy
 #Pero no me salió :(. 
 #Llamemoslo experimento 1 (de 4)
-exp1 = Sequentials([
+exp1 = Sequential([
     Dense(30, activation='sigmoid', input_shape=(784,)),
     Dense(10,activation='softmax') 
       ])
-exp1.compile(loss='categorical_crossentropy', optimizer=Adam(leargin_rate=lr), metrics=['accuracy'])
+exp1.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=lr), metrics=['accuracy'])
 history = exp1.fit(x_trainv, y_trainc, batch_size = 10, epochs = 15, validation_data=(x_testv, y_testc))
