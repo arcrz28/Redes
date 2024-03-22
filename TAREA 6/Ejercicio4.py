855828f7d0549b848b006f47988be9796df51d21
ArCrz <arcecrz.06@gmail.com>
2024-03-21 22:50:36 -0600

    agregamos el código base

--- TAREA 6/Ejercicio4.py
+++ TAREA 6/Ejercicio4.py
@@ -14,3 +14,41 @@ from matplotlib import pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+#usemos el código base que vimos en clase de ODESolver
+
+class ODESolver(Sequential):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.mse = tf.keras.losses.MeanSquaredError()
+
+    @property
+    def metrics(self):
+      return [self.loss_tracker]
+
+    def train_step(self, data):
+         batch_size = tf.shape(data)[0]
+         min = tf.cast(tf.reduce_min(data),tf.float32)
+         max = tf.cast(tf.reduce_max(data),tf.float32)
+         x = tf.random.uniform((batch_size, 1), minval=min, maxval=max)
+
+         with tf.GradientTape() as tape:
+            tape.watch(x)
+            with tf.GradientTape() as tape2:
+                tape2.watch(x)
+                y_pred = self(x, training=True)
+            dy = tape2.gradient(y_pred, x) #derivada del modelo con respecto a entradas x
+            x_o = tf.zeros((batch_size,1)) #valor de x en condicion inicial x_0=0
+            y_o = self(x_o,training=True) #valor del modelo en en x_0
+            eq = x*dy + y_pred - tf.math.cos(x)*x**2 #Ecuacion diferencial evaluada en el modelo. Queremos que sea muy pequeno
+            ic = 0. #valor que queremos para la condicion inicial o el modelo en x_0
+            loss = self.mse(tf.cast(0., tf.float32), eq) + self.mse(tf.cast(y_o, tf.float32),ic)
+
+        # Apply grads
+         grads = tape.gradient(loss, self.trainable_variables)
+         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        #update metrics
+         self.loss_tracker.update_state(loss)
+        # Return a dict mapping metric names to current value
+         return {"loss": self.loss_tracker.result()}
