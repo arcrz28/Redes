4015b71136c546814219f6c89bacf5925c7bc6d3
ArCrz <arcecrz.06@gmail.com>
2024-03-21 22:53:52 -0600

    se agrega código de la segunda ecuación

--- TAREA 6/Ejercicio4.py
+++ TAREA 6/Ejercicio4.py
@@ -86,3 +86,44 @@ plt.show()
 
 
 #Inciso b) d^2y/dx^2 = -y con y(0)=1, y'(0)= -0.5
+
+class ODESolver2(Sequential):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.mse = tf.keras.losses.MeanSquaredError()
+
+    @property
+    def metrics(self):
+      return [self.loss_tracker]
+
+    def train_step(self, data):
+         batch_size = tf.shape(data)[0]
+         min = tf.cast(tf.reduce_min(data),tf.float32)
+         max = tf.cast(tf.reduce_max(data),tf.float32)
+         x = tf.random.uniform((batch_size, 1), minval=min, maxval=max)
+
+         with tf.GradientTape(persistent=True) as tape:
+            tape.watch(x)
+            with tf.GradientTape(persistent=True) as g:
+                g.watch(x)
+                with tf.GradientTape(persistent=True) as gg:
+                    gg.watch(x)
+                    y_pred = self(x, training=True)
+                dy = gg.gradient(y_pred, x)
+            ddy = g.gradient(dy, x)
+            x_o = tf.zeros((batch_size,1)) 
+            y_o1 = self(x_o, training=True)
+            y_o2 = self(x_o, training=True)
+            eq = ddy + y_pred
+            ic1 = 1.
+            ic2 = -0.5
+            loss = self.mse(0., eq) + self.mse(y_o1, ic1) 
+
+        # Apply grads
+         grads = tape.gradient(loss, self.trainable_variables)
+         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        #update metrics
+         self.loss_tracker.update_state(loss)
+        # Return a dict mapping metric names to current value
+         return {"loss": self.loss_tracker.result()}
