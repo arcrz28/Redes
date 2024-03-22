301d75b322363b261182e9102780d491d982992f
ArCrz <arcecrz.06@gmail.com>
2024-03-21 22:39:31 -0600

    usamos el código base que ya teníamos en clase y modificamos las funciones

--- TAREA 6/Ejercicio2.py
+++ TAREA 6/Ejercicio2.py
@@ -13,3 +13,31 @@ from matplotlib import pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+
+loss_tracker = keras.metrics.Mean(name="loss")
+class Function(Sequential):
+    @property
+    def metrics(self):
+        return [keras.metrics.Mean(name="loss")] #igual cambia el loss_tracker
+
+    def train_step(self, data):
+        def funcA(x):
+            return tf.math.sin(np.pi*x)*3
+
+        def funcB(x):
+            return 1 + 2*x + 4*x**3
+        batch_size =100 #Calibra la resolucion
+        x = tf.random.uniform((batch_size,1), minval=-1, maxval=1)
+        f = funcB(x)
+
+
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = tf.math.reduce_mean(tf.math.square(y_pred-f))
+
+        grads = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        #actualiza metricas
+        loss_tracker.update_state(loss)
+
+        return {"loss": loss_tracker.result()}
